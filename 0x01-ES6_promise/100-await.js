4bfca880dc9252eb4d2c77e9387ac7160dbfcca2
KVAcodes <kadiriavictor@gmail.com>
2023-12-03 12:29:50 +0000

    added task 100

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,11 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default async function asyncUploadUser() {
+  try {
+    const uploadPhotoResponse = await uploadPhoto();
+    const createUserResponse = await createUser();
+    return { photo: uploadPhotoResponse, user: createUserResponse };
+  } catch (error) {
+    return { photo: null, user: null };
+  }
+}
