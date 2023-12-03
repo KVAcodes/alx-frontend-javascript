6fd864b9c0004edd84e18144b15d75f3d0616783
KVAcodes <kadiriavictor@gmail.com>
2023-12-03 08:50:07 +0000

    testing

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,9 +1,11 @@
-import { uploadPhoto, createUser } from './utils';
+import { createUser, uploadPhoto } from './utils';
 
 function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
-    .then((results) => console.log(results[0].body, results[1].firstName, results[1].lastName))
-    .catch(() => console.error('Signup system offline'));
+    .then((data) => {
+     console.log(`${data[0].body} ${data[1].firstName} ${data[1].lastName}`);
+    })
+    .catch(() => console.log('Signup system offline'));
 }
 
 export default handleProfileSignup;
